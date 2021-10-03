aaa6d51a6fd2c19da4349f78be6e18530debf7d2
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2021-10-03 15:56:28 +0200

    Add comments

--- utils/download.js
+++ utils/download.js
@@ -3,6 +3,7 @@
 // --- node builtins
 const https = require('https');
 
+// Download the provided URL, and run the success callback with the body. If an error occurs, the error callback is ran instead
 exports.download = function(url, success, error) {
     https.get(url, (resp) => {
         let data = '';
--- utils/pretty-numbers.js
+++ utils/pretty-numbers.js
@@ -1,5 +1,6 @@
 'use-strict';
 
+// Add a zero to the start of a number if it equals less than 10. Used to show prettier minutes.
 exports.pretty = function(nb) {
     if(nb < 10) {
         return "0" + nb;
